768343cbd4b05439ef8807b93d4a127b767d4b77
Suriya-K7 <usraising@gmail.com>
2023-09-19 10:38:22 +0530

    Demo credentials added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -83,6 +83,13 @@ const Login = () => {
               >
                 Not Register? Sign up
               </Link>
+              <button
+                className='btn mt-2 instruction forgot'
+                data-bs-toggle='modal'
+                data-bs-target='#myModal'
+              >
+                Demo Credentials
+              </button>
             </div>
           </div>
         </div>
@@ -92,6 +99,48 @@ const Login = () => {
             className='banner'
             alt='..'
           />
+        </div>
+      </div>
+      <div
+        className='modal'
+        id='myModal'
+      >
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h4 className='modal-title'>Welcome to Zen Student Dashboard</h4>
+            </div>
+            <div className='modal-body'>
+              for Student Login use below ID or create new one: <br />
+              <span className='mx-5'> Email : usraising@gmail.com</span> <br />
+              <span className='mx-5'> password : suriya@123</span> <br />
+              <span className='mx-5'> Email : usraise@live.com</span> <br />
+              <span className='mx-5'> password : suriya@123</span> <br />
+              <hr />
+              for Mentor Login use below ID
+              <br />
+              <span className='mx-5'> Email : mentor@gmail.com</span> <br />
+              <span className='mx-5'> password : mentor@123</span> <br />
+              <hr />
+              <span className='text-secondary text-justify'>
+                Note: This Student Dashboard project includes features tailored
+                for students, such as attending classes, submitting tasks,
+                checking the dashboard, submitting webcode and capstone
+                projects, raising queries, submitting porfolio, and requesting
+                leave. Mentor login provides exclusive access for task
+                evaluation only.
+              </span>
+            </div>
+            <div className='modal-footer'>
+              <button
+                type='button'
+                className='btn btn-danger'
+                data-bs-dismiss='modal'
+              >
+                Close
+              </button>
+            </div>
+          </div>
         </div>
       </div>
       <ToastContainer
